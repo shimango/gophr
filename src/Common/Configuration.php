6654b0abaa1775b4458df6d5e0b81a10e440e0f2
Jairo Kasmierchcki <jairofrasson@gmail.com>
2022-07-28 08:22:24 +0100

    Updated the README file

--- src/Common/Configuration.php
+++ src/Common/Configuration.php
@@ -3,10 +3,8 @@ namespace Shimango\Gophr\Common;
 
 /**
  * Configuration Class
- *
- * @category Class
- * @package  Gophr
- * @author   jairo.kasmierchcki@gophr.it
+  * @category Class
+ * @package  Shimango\Gophr
  */
 class Configuration
 {
